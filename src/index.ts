dcadfacbb4fbe2f6a7fea32219aa7a27b0abf67a
Tomoyuki Hata <hato6502@gmail.com>
2020-07-11 02:46:54 +0900

    Replace acorn to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import acorn from 'acorn'
+import fs from 'fs'
+import ts from 'typescript'
 
-console.log(acorn.parse('1 + 1'))
+const sourceFile = ts.createSourceFile(
+  'foo.tsx',
+  fs.readFileSync('src/example.txt').toString(),
+  ts.ScriptTarget.Latest,
+  true
+)
+
+console.log(sourceFile)
