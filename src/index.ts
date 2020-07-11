4b700f48696681b50c5513ac1bd88b61546b8897
Tomoyuki Hata <hato6502@gmail.com>
2020-07-11 14:59:21 +0900

    Implement currently

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,77 @@
-import fs from 'fs'
-import ts from 'typescript'
+import { ASTNodeTypes } from '@textlint/ast-node-types'
+import type { TxtParentNode } from '@textlint/ast-node-types'
+import type {
+  TextlintPluginCreator,
+  TextlintPluginOptions,
+  TextlintPluginProcessor,
+} from '@textlint/types'
+import * as ts from 'typescript'
 
-const sourceFile = ts.createSourceFile(
-  'foo.tsx',
-  fs.readFileSync('src/example.txt').toString(),
-  ts.ScriptTarget.Latest,
-  true
-)
+const jsxToAST = (node: ts.Node): TxtParentNode => {
+  const startLineAndCharacter = node
+    .getSourceFile()
+    .getLineAndCharacterOfPosition(node.pos)
+  const endLineAndCharacter = node
+    .getSourceFile()
+    .getLineAndCharacterOfPosition(node.end)
 
-console.log(sourceFile)
+  return {
+    // TODO: Implement map for all SyntaxKinds.
+    type:
+      node.kind === ts.SyntaxKind.JsxText
+        ? ASTNodeTypes.Str
+        : ASTNodeTypes.HtmlBlock,
+    raw: node.getText(),
+    range: [node.pos, node.end],
+    loc: {
+      start: {
+        column: startLineAndCharacter.character,
+        line: startLineAndCharacter.line + 1,
+      },
+      end: {
+        column: endLineAndCharacter.character,
+        line: endLineAndCharacter.line + 1,
+      },
+    },
+    children: node.getChildren().map(jsxToAST),
+  }
+}
+
+class JSXProcessor implements TextlintPluginProcessor {
+  extensions: string[]
+
+  constructor(options?: TextlintPluginOptions) {
+    this.extensions = options?.extensions ?? []
+  }
+
+  availableExtensions() {
+    return ['.jsx', '.tsx', ...this.extensions]
+  }
+
+  processor() {
+    return {
+      preProcess(text: string, filePath?: string) {
+        const sourceFile = ts.createSourceFile(
+          filePath ?? 'foo.tsx',
+          text,
+          ts.ScriptTarget.Latest,
+          true
+        )
+
+        return jsxToAST(sourceFile)
+      },
+      postProcess(messages: any[], filePath?: string) {
+        return {
+          messages,
+          filePath: filePath ?? '<jsx>',
+        }
+      },
+    }
+  }
+}
+
+const creator: TextlintPluginCreator = {
+  Processor: JSXProcessor,
+}
+
+export default creator
