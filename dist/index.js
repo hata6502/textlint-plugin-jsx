e21bc2f08eeeeb27ff2a1c243fee740aa4f20e99
Tomoyuki Hata <hato6502@gmail.com>
2020-07-12 20:14:31 +0900

    Release v1.0.3

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var JSXProcessor_1 = require("./JSXProcessor");
-var creator = {
-    Processor: JSXProcessor_1.default,
-};
-exports.default = creator;
-//# sourceMappingURL=index.js.map
